6af0aaed0d74819e3caaf6375f098bef9b4c11ac
nowoadisuryo <nowoadisuryo@gmail.com>
2022-12-01 15:47:14 +0700

    Updated tests/Constant

--- src/tests/Constant.ts
+++ src/tests/Constant.ts
@@ -1,4 +1,5 @@
 import UserInterface from "../interfaces/UserInterface.js";
+import ContactInterface from "../interfaces/ContactInterface.js";
 
 export const user: UserInterface = {
     email: 'user@example.com',
@@ -8,4 +9,11 @@ export const user: UserInterface = {
 export const admin: UserInterface = {
     email: 'johndoe@example.com',
     password: 'okemantap'
+}
+
+export const contact1: ContactInterface = {
+    full_name: 'John Doe',
+    phone_number: '+628123839293812',
+    address: 'Jakarta',
+    profile_pic: ''
 }
