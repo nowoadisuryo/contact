bc293ad7ef1c658a3d1ed8b337c86776eb3807b4
nowoadisuryo <nowoadisuryo@gmail.com>
2022-12-09 16:47:43 +0700

    Added stop all task script

---
+++ scripts/stop-all-task.sh
@@ -0,0 +1,6 @@
+OLD_TASK_IDS=$(aws ecs list-tasks --cluster contact-app-cluster --desired-status RUNNING --family contact-app | egrep "task/" | sed -E "s/.*task\/(.*)\"/\1/" | sed -z 's/\n/ /g')
+IFS=', ' read -r -a array <<< "$OLD_TASK_IDS"
+for element in "${array[@]}"
+do
+    aws ecs stop-task --cluster contact-app-cluster --task ${element}
+done
